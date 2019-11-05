8cf31fd7ba4c75df2c8ec446ed85dc4427637051
Habib Benharrat <benharrat.habib1@gmail.com>
2019-11-05 11:00:06 +0100

    main.php

---
+++ main.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+require_once 'src/mf/utils/ClassLoader.php';
+
+$autolaod = new mf\utils\ClassLoader("src");
+$autolaod->register();
+
+
+/* pour le chargement automatique des classes d'Eloquent (dans le répertoire vendor) */
+require_once 'vendor/autoload.php';
+
+$config = parse_ini_file('conf/config.ini');
+
+/* une instance de connexion  */
+$db = new Illuminate\Database\Capsule\Manager();
+
+$db->addConnection( $config ); /* configuration avec nos paramètres */
+$db->setAsGlobal();            /* rendre la connexion visible dans tout le projet */
+$db->bootEloquent();           /* établir la connexion */
+
+
+
+
