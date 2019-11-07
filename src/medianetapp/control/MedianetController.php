2e15f2e8b4af6b071721e16c03d8b7d779493a6e
jdevuono <jacques.devuono@gmail.com>
2019-11-07 16:40:02 +0100

    .

--- src/medianetapp/control/MedianetController.php
+++ src/medianetapp/control/MedianetController.php
@@ -61,24 +61,35 @@ class MedianetController extends \mf\control\AbstractController
 
     /*Action : View search form*/
     public function viewSearch(){
-        $view = new MedianetView(null);
-        $view->render("search");
+        if(isset($_SESSION['access_level']) && $_SESSION['access_level'] === MedianetAuthentification::ACCESS_LEVEL_USER){
+            $view = new MedianetView(null);
+            $view->render("search");
+        }
+        else{
+            Router::executeRoute("login");
+        }
     }
     /*Action : Search*/
     public function search(){
-        if(isset($_REQUEST["txtKey"])&&isset($_REQUEST["txtType"])&&isset($_REQUEST["txtKind"])){
+        if(isset($_SESSION['access_level']) && $_SESSION['access_level'] === MedianetAuthentification::ACCESS_LEVEL_USER) {
 
-            /*Filtred input*/
-            $filtredKey=filter_var($_REQUEST["txtKey"],FILTER_SANITIZE_STRING);
+            if (isset($_REQUEST["txtKey"]) && isset($_REQUEST["txtType"]) && isset($_REQUEST["txtKind"])) {
 
-            /*Get documents*/
-            $documents = Document::where('title','like',$filtredKey.'%')
-                ->orWhere('description','like','%'.$filtredKey.'%')
-                ->where('id_Type','=',$_REQUEST["txtType"])
-                ->where('id_Kind','=',$_REQUEST["txtKind"])
-                ->get();
-            $view = new MedianetView($documents);
-            $view->render("catalogue");
+                /*Filtred input*/
+                $filtredKey = filter_var($_REQUEST["txtKey"], FILTER_SANITIZE_STRING);
+
+                /*Get documents*/
+                $documents = Document::where('title', 'like', $filtredKey . '%')
+                    ->orWhere('description', 'like', '%' . $filtredKey . '%')
+                    ->where('id_Type', '=', $_REQUEST["txtType"])
+                    ->where('id_Kind', '=', $_REQUEST["txtKind"])
+                    ->get();
+                $view = new MedianetView($documents);
+                $view->render("catalogue");
+            }
+        }
+        else{
+            Router::executeRoute("login");
         }
     }
     /* PERMET DE CREE UN MESSAGE D'ERREUR ET D'AFFICHER LES SOURCES DE L'ERREUR */
